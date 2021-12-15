d6397ddc9935649f4344744919b17b9053b711b4
AlexRazor1337 <alexrazor1337@gmail.com>
2021-12-15 12:23:12 +0200

    Convert calendar to FC

--- frontend/src/MainCalendar/MainCalendar.jsx
+++ frontend/src/MainCalendar/MainCalendar.jsx
@@ -1,59 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Calendar, Views, momentLocalizer } from 'react-big-calendar'
 import moment from 'moment'
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import events from './events'
 import ExampleControlSlot from './ExampleControlSlot'
-const propTypes = {}
 
-
-class MainCalendar extends React.Component { // TODO Make functional component, add spinner, load events
-    constructor(...args) {
-        super(...args)
-
-        this.state = { events }
-    }
-
-    handleSelect = ({ start, end }) => {
+export default function MainCalendar(props) { // TODO add spinner, load events
+    const [calendarEvents, setEvents] = useState(events)
+    const handleSelect = ({ start, end }) => {
         const title = window.prompt('New Event name')
         if (title)
-            this.setState({
-                events: [
-                    ...this.state.events,
-                    {
-                        start,
-                        end,
-                        title,
-                    },
-                ],
-            })
+        setEvents([
+            ...calendarEvents,
+            {
+                start,
+                end,
+                title,
+            },
+        ])
     }
 
-    render() {
-        const localizer = momentLocalizer(moment)
-        return (
-            <>
-                <ExampleControlSlot.Entry waitForOutlet>
-                    <strong>
-                        Click an event to see more info, or drag the mouse over the calendar
-                        to select a date/time range.
-                    </strong>
-                </ExampleControlSlot.Entry>
-                <Calendar
-                    MainCalendar
-                    localizer={localizer}
-                    events={this.state.events}
-                    defaultView={Views.WEEK}
-                    scrollToTime={new Date(1970, 1, 1, 6)}
-                    defaultDate={new Date(2015, 3, 12)}
-                    onSelectEvent={event => alert(event.title)}
-                    onSelectSlot={this.handleSelect}
-                />
-            </>
-        )
-    }
-}
-
-MainCalendar.propTypes = propTypes
-
-export default MainCalendar
+    const localizer = momentLocalizer(moment)
+    return (
+        <>
+            <ExampleControlSlot.Entry waitForOutlet>
+                <strong>
+                    Click an event to see more info, or drag the mouse over the calendar
+                    to select a date/time range.
+                </strong>
+            </ExampleControlSlot.Entry>
+            <Calendar
+                MainCalendar
+                localizer={localizer}
+                events={calendarEvents}
+                defaultView={Views.WEEK}
+                scrollToTime={new Date(1970, 1, 1, 6)}
+                defaultDate={new Date(2015, 3, 12)}
+                onSelectEvent={event => alert(event.title)}
+                onSelectSlot={handleSelect}
+            />
+        </>
+    )
+};
